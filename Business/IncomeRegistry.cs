631d097e6541e944a179990c4b13a530bb0d27eb
Demon6590 <dima.chernytin@gmail.com>
2026-03-23 12:48:10 +0300

    добавление Реестр доходов.

---
+++ Business/IncomeRegistry.cs
@@ -0,0 +1,59 @@
+﻿namespace Business;
+
+public class IncomeRegistry
+{
+    public List<Income> Incomes { get; }=new List<Income>();
+    
+    public void AddIncome(Income income)=> Incomes.Add(income);
+
+    public List<Income> getIncomesByMonth(int year, int month)
+    {
+        List<Income> monthlyIncomes = new List<Income>();
+    
+        foreach (Income income in Incomes)
+        {
+            if (income.date.Year == year && income.date.Month == month)
+            {
+                monthlyIncomes.Add(income);
+            }
+        }
+    
+        return monthlyIncomes;
+    }
+    public double getTotalIncomeByMonth(int year, int month)
+    {
+        double totalIncome = 0.0;
+    
+        foreach (Income income in Incomes)
+        {
+            if (income.date.Year == year && income.date.Month == month)
+            {
+                totalIncome += income.amount;
+            }
+        }
+        return totalIncome;
+    }
+
+    public double getTaxByMonth(int year, int month)
+    {
+        double tax = 0.0;
+        foreach (Income income in Incomes)
+        {
+            if(income.date.Year == year && income.date.Month == month)
+            {
+                tax += income.calculateTax();
+            }
+        }
+        return tax;
+    }
+
+    public double getProfitByMonth(int year, int month)
+    {
+        double totalIncome = getTotalIncomeByMonth(year, month);
+        double totalTax = getTaxByMonth(year, month);
+    
+        double profit = totalIncome - totalTax;
+        return profit;
+    }
+
+}
